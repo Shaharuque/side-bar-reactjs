46fc60a8c48a1bb185e11d54a450bb1e7b2cb917
unknown <shahrukhamin27@gmail.com>
2022-04-21 01:45:07 +0600

    second

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import './sidemenu.css'
 import logo from '../assets/logo/webscript.png'
+import user from '../assets/user.jpg'
+import MenuItem from './subMenu/MenuItem';
+
 
 const SideMenu = () => {
-    const [inactive,setInactive]=useState(false)
+    const [inactive, setInactive] = useState(false)
 
 
     return (
         <div className={`side-menu ${inactive ? "inactive" : ""}`}>
             <div className='top-section'>
                 <div className='logo'>
-                    <img src={logo} alt=""  />
+                    <img src={logo} alt="" />
                 </div>
-                <div onClick={()=>setInactive(!inactive)} className='toggle-menu-btn'>
-                    <i class="bi bi-arrow-left-square-fill"></i>
+                <div onClick={() => setInactive(!inactive)} className='toggle-menu-btn'>
+                    {inactive ? <i class="bi bi-arrow-right-square-fill"></i> : <i class="bi bi-arrow-left-square-fill"></i>}
                 </div>
             </div>
 
@@ -25,6 +28,50 @@ const SideMenu = () => {
             </div>
 
             <div className='divider'></div>
+
+            <div className="main-menu">
+                <ul>
+                    <li>
+                        <a className='menu-item' >
+                            <div className='menu-icon'>
+                                <i class="bi bi-speedometer2"></i>
+                            </div>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+
+                   <MenuItem name={'Design'} subMenus={[{name:'Courses'},{name:'Graphics'}]}></MenuItem>
+
+                    <li>
+                        <a className='menu-item' >
+                            <div className='menu-icon'>
+                                <i class="bi bi-bar-chart"></i>
+                            </div>
+                            <span>Charts</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className='menu-item' >
+                            <div className='menu-icon'>
+                                <i class="bi bi-app-indicator"></i>
+                            </div>
+                            <span>Profits</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div className='side-menu-footer'>
+                <div className="avatar">
+                    <img src={user} alt="" />
+                </div>
+                <div className="user-info">
+                    <h4>Adward Oma</h4>
+                    <p>adward444@gmail.com</p>
+                </div>
+            </div>
         </div>
     );
 };
